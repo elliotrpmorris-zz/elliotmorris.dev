1a9a701955ad00b5734d8a129601307e2c050fa1
Elliot <elliot@ayup.agency>
2019-03-15 09:32:10 +0000

    added fluid

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,12 +1,28 @@
 <template>
-  <div class="grid-container">
+  <div class="grid-container" :class="classes">
     <slot/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'container',
+  name: "container",
+
+  props: {
+    fluid: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+
+  computed: {
+    classes() {
+      return {
+        "grid-container--fluid": this.fluid
+      };
+    }
+  }
 };
 </script>
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,46 @@
 <template>
-  <div class="about"></div>
+  <div class="about">
+    <em-container class="about__outer" fluid>
+      <em-container class="about__inner">
+        <em-row>
+          <em-column
+            class="about__content"
+            twelve
+          >I am a full stack developer from the UK, currently working for a Marketing agency in Leeds aswell as working on my own projects. I work on both creating websites and software. My experience ranges from developing scripts, (for example to access an API) suggesting optimization improvements, to building full blown websites with or without a CMS and hosting the site I have knowledge of both linux and windows servers.</em-column>
+        </em-row>
+      </em-container>
+    </em-container>
+  </div>
 </template>
 
 <script>
+import EmContainer from "@/components/Container.vue";
+import EmRow from "@/components/Row.vue";
+import EmColumn from "@/components/Column.vue";
+
 export default {
-  name: "about"
+  name: "about",
+  components: {
+    EmContainer,
+    EmRow,
+    EmColumn
+  }
 };
 </script>
+
+<style scoped lang="scss">
+@import "../assets/scss/app";
+.about {
+  &__outer {
+    background-color: $alto;
+  }
+  &__inner {
+    background-color: $dove-grey;
+  }
+  &__content {
+    color: $white;
+    font-family: $font-primary;
+    padding: map-get($map: $spaceamounts, $key: 32);
+  }
+}
+</style>
