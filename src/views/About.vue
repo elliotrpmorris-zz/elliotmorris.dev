880fa806d4086fecad9028ea11bb059d025153b4
Elliot <elliot@ayup.agency>
2019-04-15 09:37:51 +0100

    updated about page remove border

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,34 +1,32 @@
 <template>
-  <em-container class="about__outer" fluid>
-    <em-container class="about__inner">
-      <em-row>
-        <em-column class="about__content" twelve>
-          <em-card alto>
-            <em-heading mineShaft xlarge>About me</em-heading>
-            <em-page-text mineShaft>
-              I am a full stack developer from the UK,
-              currently working for a tech for good agency in Leeds.
-              Working on creating web apps and working with the latest tech.
-            </em-page-text>
-            <em-page-text mineShaft>
-              As an advocate for all things web it is important to me
-              that the product is usable by all, ensuring
-              I am following high accessibility standards
-              as well as achieving the best performance and producing
-              top quality code.
-            </em-page-text>
-            <em-heading mineShaft>Heres some tech that i like to use:</em-heading>
-            <ul>
-              <li>HTML5, CSS3, JS</li>
-              <li>Vue.js</li>
-              <li>React</li>
-              <li>.Net (C#), Umbraco</li>
-              <li>Php, Laravel, Wordpress, Statamic</li>
-            </ul>
-          </em-card>
-        </em-column>
-      </em-row>
-    </em-container>
+  <em-container>
+    <em-row>
+      <em-column class="about__content" twelve>
+        <em-card alto>
+          <em-heading mineShaft xlarge>About me</em-heading>
+          <em-page-text mineShaft>
+            I am a full stack developer from the UK,
+            currently working for a tech for good agency in Leeds.
+            Working on creating web apps and working with the latest tech.
+          </em-page-text>
+          <em-page-text mineShaft>
+            As an advocate for all things web it is important to me
+            that the product is usable by all, ensuring
+            I am following high accessibility standards
+            as well as achieving the best performance and producing
+            top quality code.
+          </em-page-text>
+          <em-heading mineShaft>Heres some tech that i like to use:</em-heading>
+          <ul>
+            <li>HTML5, CSS3, JS</li>
+            <li>Vue.js</li>
+            <li>React</li>
+            <li>.Net (C#), Umbraco</li>
+            <li>Php, Laravel, Wordpress, Statamic</li>
+          </ul>
+        </em-card>
+      </em-column>
+    </em-row>
   </em-container>
 </template>
 
@@ -57,14 +55,6 @@ export default {
 @import "../assets/scss/app";
 
 .about {
-  &__outer {
-    background-color: $alto;
-  }
-
-  &__inner {
-    background-color: $dove-grey;
-  }
-
   &__content {
     color: $white;
     font-family: $font-primary;
